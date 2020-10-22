beb510c6819ad03d27c2a0c0cb721ac802184ea2
v_yuanymeng <v_yuanymeng@tencent.com>
2020-10-22 23:52:07 +0800

    gettingConnectErrors

--- DCM/DCM_display.py
+++ DCM/DCM_display.py
@@ -1,67 +1,67 @@
 from tkinter import *
-#code for DCM GUI
+# code for DCM GUI
 
-# class DCMWindow:
-#     def __init__(self, master):
-#         self.master = master
-#         master.title("DCM")
+
 class ConnectDevice:
     def __init__(self, master):
-        # self.master = master
-        # master.title("Choose Connection Option")
-        top=self.top=Toplevel(master)
+        top = self.top = Toplevel(master)
         top.title("Choose Connection Option")
-        
 
-        self.comfirmAccButton = Button(top, text="Connect Current Device", command=self._connectCurrDevice)
-        self.comfirmAccButton.grid(row=2, column=0, pady=10, padx=10, columnspan=2)
-    
+        self.connectCurrButton = Button(
+            top, text="Connect Current Device", command=self._connectCurrDevice)
+        self.connectCurrButton.grid(
+            row=2, column=0, pady=10, padx=10, columnspan=2)
+        self.pairNewButton = Button(
+            top, text="Pair New Device", command=self._pairNewDevice)
+        self.pairNewButton.grid(
+            row=3, column=0, pady=10, padx=10, columnspan=2)
+
     def _connectCurrDevice(self):
+        self.popupmsg("Communicating...")
+
+    def _pairNewDevice(self):
         # self.refresh()
-        self.master.destroy()
-        # Label(top, text="Connect Communicating to Device...").grid(row=0, pady=10, padx=10)
-    
+        self.popupmsg("Finding New Device...")
+
     def popupmsg(self, msg):
         popup = Tk()
         popup.wm_title("!")
         label = Label(popup, text=msg)
         label.pack(side="top", fill="x", pady=10)
-        B1 = Button(popup, text="Okay", command = popup.destroy)
+        B1 = Button(popup, text="Okay", command=popup.destroy)
         B1.pack(pady=5, padx=10)
         popup.mainloop()
 
-    def refresh(self,master):
-        self.top.destroy()
-        top=self.top=Toplevel(master)
-        top.title("Choose Connection Option")
-       
+    # def refresh(self,master):
+    #     self.top.destroy()
+    #     top=self.top=Toplevel(master)
+    #     top.title("Choose Connection Option")
+
+
 class DCMWindow:
     def __init__(self, master):
         self.master = master
         master.title("DCM")
 
-        self.loginButton = Button(master, text="Connect My Device", command=self.connectOptions)
-        self.loginButton.grid(row=3, column=0, columnspan = 2, pady=2, padx=10)
+        self.DCMButton = Button(
+            master, text="Connect My Device", command=self.connectOptions)
+        self.DCMButton.grid(row=3, column=0, columnspan=2, pady=2, padx=10)
 
     def connectOptions(self):
         try:
-            newAcc = ConnectDevice(self.master)
-            self.createAccButton['state'] = 'disabled'
-            self.master.wait_window(newAcc.top)
-            self.createAccButton['state'] = 'normal'
+            connect = ConnectDevice(self.master)
+            self.DCMButton['state'] = 'disabled'
+            self.master.wait_window(connect.top)
+            self.DCMButton['state'] = 'normal'
+            TRUE = FALSE
         except:
             self.popupmsg("connectOptions Error")
-    
+
     def popupmsg(self, msg):
         popup = Tk()
         popup.wm_title("!")
         label = Label(popup, text=msg)
         label.pack(side="top", fill="x", pady=10)
-        B1 = Button(popup, text="Okay", command = popup.destroy)
+        B1 = Button(popup, text="Okay", command=popup.destroy)
         B1.pack(pady=5, padx=10)
         popup.mainloop()
-
-#Testin
-#login = Tk()
-#oginWindow = loginWindow(login)
-#login.mainloop()
--- DCM/DCM_main.py
+++ DCM/DCM_main.py
@@ -3,11 +3,13 @@ import DCM_login
 import DCM_display
 
 #Login Window
-# login = Tk()
-# loginWindow = DCM_login.loginWindow(login)
-# login.mainloop()
+login = Tk()
+loginWindow = DCM_login.loginWindow(login)
+login.mainloop()
+
 
 #DCM Display
+#this should not be called when login window is closed, needs be fixed
 DCM = Tk()
 DCMWindow = DCM_display.DCMWindow(DCM)
 DCM.mainloop()
