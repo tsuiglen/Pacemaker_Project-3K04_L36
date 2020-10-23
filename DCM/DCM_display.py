6bb83f6b3c584577da17eabc412da126b42785a1
v_yuanymeng <v_yuanymeng@tencent.com>
2020-10-23 19:06:08 +0800

    added AAI VVI

--- DCM/DCM_display.py
+++ DCM/DCM_display.py
@@ -125,8 +125,16 @@ class DCM_AAI(tk.Frame):
         tk.Label(self, text="Upper Rate Limit").grid(row=1, column=0, pady=10, padx=10)
         tk.Label(self, text="Lower Rate Limit").grid(row=1, column=2, pady=10, padx=10)
 
-        tk.Label(self, text="Ventricular Amplitude").grid(row=2, column=0, pady=10, padx=10)
-        tk.Label(self, text="Ventricular Pulse Width").grid(row=2, column=2, pady=10, padx=10)
+        tk.Label(self, text="Atrial Amplitude").grid(row=2, column=0, pady=10, padx=10)
+        tk.Label(self, text="Atrial Pulse Width").grid(row=2, column=2, pady=10, padx=10)
+
+        tk.Label(self, text="Atrial Sensitivity").grid(row=3, column=0, pady=10, padx=10)
+        tk.Label(self, text="ARP").grid(row=3, column=2, pady=10, padx=10)
+
+        tk.Label(self, text="PVARP").grid(row=4, column=0, pady=10, padx=10)
+        tk.Label(self, text="Hysteresis").grid(row=4, column=2, pady=10, padx=10)
+
+        tk.Label(self, text="Rate Smoothing").grid(row=5, column=0, pady=10, padx=10)
 
         self.upperRateLimit = tk.Entry(self)
         self.upperRateLimit.grid(row=1, column=1, pady=10, padx=10)
@@ -134,11 +142,26 @@ class DCM_AAI(tk.Frame):
         self.lowerRateLimit = tk.Entry(self)
         self.lowerRateLimit.grid(row=1, column=3, pady=10, padx=10)
 
-        self.ventricularPulseAmplitude = tk.Entry(self)
-        self.ventricularPulseAmplitude.grid(row=2, column=1, pady=10, padx=10)
+        self.atrialPulseAmplitude = tk.Entry(self)
+        self.atrialPulseAmplitude.grid(row=2, column=1, pady=10, padx=10)
 
-        self.ventricularPulseWidth = tk.Entry(self)
-        self.ventricularPulseWidth.grid(row=2, column=3, pady=10, padx=10)
+        self.atrialPulseWidth = tk.Entry(self)
+        self.atrialPulseWidth.grid(row=2, column=3, pady=10, padx=10)
+
+        self.atrialSensitivity = tk.Entry(self)
+        self.atrialSensitivity.grid(row=3, column=1, pady=10, padx=10)
+
+        self.ARP = tk.Entry(self)
+        self.ARP.grid(row=3, column=3, pady=10, padx=10)
+
+        self.PVARP = tk.Entry(self)
+        self.PVARP.grid(row=4, column=1, pady=10, padx=10)
+
+        self.hysteresis = tk.Entry(self)
+        self.hysteresis.grid(row=4, column=3, pady=10, padx=10)
+
+        self.rateSmoothing = tk.Entry(self)
+        self.rateSmoothing.grid(row=5, column=1, pady=10, padx=10)
 
     def getDropDown(self, val):
         self.master.switch_frame(switchMode(val))
@@ -164,6 +187,13 @@ class DCM_VVI(tk.Frame):
         tk.Label(self, text="Ventricular Amplitude").grid(row=2, column=0, pady=10, padx=10)
         tk.Label(self, text="Ventricular Pulse Width").grid(row=2, column=2, pady=10, padx=10)
 
+        tk.Label(self, text="Ventricular Sensitivity").grid(row=3, column=0, pady=10, padx=10)
+        tk.Label(self, text="VRP").grid(row=3, column=2, pady=10, padx=10)
+
+        tk.Label(self, text="Hysteresis").grid(row=4, column=0, pady=10, padx=10)
+        tk.Label(self, text="Rate Smoothing").grid(row=4, column=2, pady=10, padx=10)
+
+
         self.upperRateLimit = tk.Entry(self)
         self.upperRateLimit.grid(row=1, column=1, pady=10, padx=10)
 
@@ -175,6 +205,18 @@ class DCM_VVI(tk.Frame):
 
         self.ventricularPulseWidth = tk.Entry(self)
         self.ventricularPulseWidth.grid(row=2, column=3, pady=10, padx=10)
+
+        self.ventricularSensitivity = tk.Entry(self)
+        self.ventricularSensitivity.grid(row=3, column=1, pady=10, padx=10)
+
+        self.VRP = tk.Entry(self)
+        self.VRP.grid(row=3, column=3, pady=10, padx=10)
+
+        self.hysteresis = tk.Entry(self)
+        self.hysteresis.grid(row=4, column=1, pady=10, padx=10)
+
+        self.rateSmoothing = tk.Entry(self)
+        self.rateSmoothing.grid(row=4, column=3, pady=10, padx=10)
 
     def getDropDown(self, val):
         self.master.switch_frame(switchMode(val))
